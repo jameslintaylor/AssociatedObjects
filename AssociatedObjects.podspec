da5285bd0c5ddb6f7a45619fc0ad49d76cdaf9ab
James Taylor <JLT2@JLT-2.local>
2016-09-12 12:34:45 -0400

    added api for string keys

--- AssociatedObjects.podspec
+++ AssociatedObjects.podspec
@@ -17,7 +17,7 @@ Pod::Spec.new do |s|
   s.author = "James Taylor"
   s.social_media_url   = "http://twitter.com/jameslintaylor"
 
-  s.ios.deployment_target = "8.0"
+  s.ios.deployment_target = "9.0"
   s.osx.deployment_target = "10.9"
   s.watchos.deployment_target = "2.0"
   s.tvos.deployment_target = "9.0"
--- AssociatedObjects/AssociatedObjects.swift
+++ AssociatedObjects/AssociatedObjects.swift
@@ -23,18 +23,64 @@ public enum AssociationPolicy: UInt {
 
 public protocol AssociatedObjects: class {}
 
+// transparent wrappers
+
 public extension AssociatedObjects {
     /// wrapper around `objc_getAssociatedObject`
-    func getAssociatedObject(key: UnsafeRawPointer) -> AnyObject? {
-        return objc_getAssociatedObject(self, key) as AnyObject
+    func ao_get(pkey: UnsafeRawPointer) -> Any? {
+        return objc_getAssociatedObject(self, pkey)
     }
     /// wrapper around `objc_setAssociatedObject`
-    func setAssociatedObject(_ object: AnyObject, key: UnsafeRawPointer, policy: AssociationPolicy = .retainNonatomic) {
-        objc_setAssociatedObject(self, key, object, policy.objc)
+    func ao_set(_ value: Any, pkey: UnsafeRawPointer, policy: AssociationPolicy = .retainNonatomic) {
+        objc_setAssociatedObject(self, pkey, value, policy.objc)
     }
     /// wrapper around 'objc_removeAssociatedObjects'
-    func removeAssociatedObjects() {
+    func ao_removeAll() {
         objc_removeAssociatedObjects(self)
+    }
+}
+
+// string key managed wrappers
+
+fileprivate class MutableBox<T> {
+    private(set) var value: T
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    // accepts a closure that can mutate `value` from within
+    func mutate(f: (inout T) throws -> ()) rethrows {
+        try f(&value)
+    }
+}
+
+fileprivate typealias KeyBox = MutableBox<[String: UnsafePointer<Int8>]>
+fileprivate var keyBoxKey = "_" as Character
+
+fileprivate extension AssociatedObjects {
+    // the box containing the dictionary we'll use to manage string keys
+    fileprivate var keybox: KeyBox {
+        return ao_get(pkey: &keyBoxKey) as? KeyBox ?? {
+            let keybox = KeyBox([:])
+            ao_set(keybox, pkey: &keyBoxKey)
+            return keybox
+        }()
+    }
+}
+
+extension AssociatedObjects {
+    /// `String` key managed wrapper around `objc_setAssociatedObject`
+    func ao_set(_ value: Any, key: String) {
+        key.withCString { p in
+            ao_set(value, pkey: p)
+            // store the key
+            keybox.mutate { $0[key] = p }
+        }
+    }
+    /// `String` key managed wrapper around `objc_getAssociatedObject`
+    func ao_get(key: String) -> Any? {
+        guard let p = keybox.value[key] else { return nil }
+        return ao_get(pkey: p)
     }
 }
 
